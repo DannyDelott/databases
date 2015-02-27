f660660b1f0cd85b55755642cf05f53aa602f6d1
DannyDelott <dannydelott@gmail.com>
2015-02-27 15:42:28 -0800

    Create schema
    - Make a database called 'chat'.
    - Make a table for users, messages, and rooms.

--- server/schema.sql
+++ server/schema.sql
@@ -1,9 +1,23 @@
+DROP DATABASE IF EXISTS chat;
 CREATE DATABASE chat;
 
 USE chat;
 
+CREATE TABLE users (
+  id int NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  user varchar(25)
+);
+
+CREATE TABLE rooms (
+  id int NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  room varchar(25)
+);
+
 CREATE TABLE messages (
-  /* Describe your table here.*/
+  userId int NOT NULL,
+  roomId int NOT NULL,
+  message varchar(255),
+  postedAt date
 );
 
 /* Create other tables and define schemas for them here! */
