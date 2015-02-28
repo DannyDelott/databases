c65fd745e0b9bb73b1cd8c17756ec9b32568fe2f
DannyDelott <dannydelott@gmail.com>
2015-02-27 17:34:20 -0800

    Create helper functions

--- server/db/index.js
+++ server/db/index.js
@@ -1,20 +1,40 @@
 var mysql = require('mysql');
 
-// Create a database connection and export it from this file.
-// You will need to connect with the user "root", no password,
-// and to the database "chat".
-
+// initialize the database connection
 var connection = mysql.createConnection({
   host     : 'localhost',
   user     : 'root',
   password : '',
   database : 'chat'
 });
-
 connection.connect();
 
-connection.query('SELECT user FROM users', function(err, rows, fields){
-  if(err) throw err;
-  console.log('fields', fields);
-  console.log(JSON.stringify(rows));
-});
+module.exports.getUsers = function(callback){
+  connection.query('SELECT user FROM users', function(err, rows, fields){
+    if(err) throw err;
+    callback(rows);
+  });
+};
+
+module.exports.getMessages = function(callback){
+  connection.query('SELECT message FROM messages', function(err, rows, fields){
+    if(err) throw err;
+    callback(rows);
+  });
+};
+
+module.exports.getRooms = function(callback){
+  connection.query('SELECT room FROM rooms', function(err, rows, fields){
+    if(err) throw err;
+    callback(rows);
+  });
+};
+
+// pass in user as object {user: 'name'}
+module.exports.insertUser = function(user){
+  connection.query('INSERT INTO users', user, function(err, result){
+    if(err) throw err;
+    console.log(result);
+  })
+};
+
