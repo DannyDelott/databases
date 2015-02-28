a407d94867b7b17cb31244b74216cffef8599c40
DannyDelott <dannydelott@gmail.com>
2015-02-27 20:00:55 -0800

    Pseudocode request flow
    - Minor changes made to facilitate.

--- server/models/index.js
+++ server/models/index.js
@@ -1,11 +1,17 @@
 var db = require('../db');
 
-
-
+db.getUsers(function(rows){
+  console.log(JSON.stringify(rows));
+}, null);
 
 module.exports = {
   messages: {
-    get: function () {}, // a function which produces all the messages
+
+     // a function which produces all the messages
+    get: function (timestamp, callback) {
+      // use db.getMessages(callback, timestamp)
+    },
+
     post: function () {} // a function which can be used to insert a message into the database
   },
 
